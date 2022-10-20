d522c6820bd156b9973a02d12007961347bf7424
s.mirzazada <s.mirzazada@disoft.dev>
2022-10-20 18:45:14 +0400

    Добавить фильтрацию для проекта и тасков #13

--- source/main/views.py
+++ source/main/views.py
@@ -17,17 +17,20 @@ class Home(LoginRequiredMixin, ListView):
     model = Project
     context_object_name = 'projects'
 
+    def get_queryset(self):
+        return Project.objects.filter(membership__user=self.request.user)
+
 
 class ProjectDetail(LoginRequiredMixin, DetailView):
     template_name = 'project_detail.html'
     model = Project
-    slug_url_kwarg = 'slug'
-    slug_field = 'slug'
     context_object_name = 'project'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tasks'] = Task.objects.filter(project=self.object).order_by('-created_at').select_related('project')
+        context['tasks'] = Task.objects.filter(
+            assignee=self.request.user,
+            project=self.object).order_by('-created_at').select_related('project')
         return context
 
 
